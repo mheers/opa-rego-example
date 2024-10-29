6ac94adcac9967688096cc6833a6b4f8660ecd2d
Marcel Heers <info@heers.it>
2024-10-29 13:46:23 +0200

    adds a dagger/gh-actions pipeline to test, build and push an opa bundle

--- README.md
+++ README.md
@@ -24,3 +24,45 @@ This should return:
   }
 }
 ```
+
+## CI/CD Pipeline
+
+This repository contains a GitHub Actions pipeline that runs the rego tests, builds and pushes the bundle to an OCI registry.
+
+The pipeline leverages [dagger](https://dagger.io/) that allows the development of CI/CD pipelines independently of the CI/CD platform and can be run locally but also on GitHub Actions, GitLab CI, Jenkins, etc.
+
+Before running the pipeline, you need to set the following secrets in your GitHub repository:
+
+- `REGISTRY_ACCESS_TOKEN`: The access token to authenticate with the registry.
+
+Also, you need to adjust the following constants in the `ci/dagger/src/index.ts` file:
+
+```ts
+const baseImage = "mheers/opa-tools:latest"
+const registry = "registry-1.docker.io"
+const repository = "mheers/opa-policy"
+const tag = "1.0.0"
+const username = "mheers"
+```
+
+### Run the pipeline locally
+
+Before running the pipeline locally, you need to install the `dagger` CLI. You can find the installation instructions [here](https://docs.dagger.io/install).
+
+You also need to put a `.env` file in the `ci/` directory with the following content:
+
+```bash
+REGISTRY_ACCESS_TOKEN=<your-registry-access-token>
+```
+
+You can then run the pipeline locally using the following command:
+
+```bash
+cd ci/
+
+# run test pipeline
+dagger call test-regos --directory-arg ../bundle
+
+# run build and push pipeline
+dagger call test-build-and-push-bundle --directory-arg ../bundle --registry-token=env:REGISTRY_ACCESS_TOKEN
+```
---
+++ ci/dagger/src/index.ts
@@ -0,0 +1,51 @@
+/**
+ * A generated module for Ci functions
+ *
+ * This module has been generated via dagger init and serves as a reference to
+ * basic module structure as you get started with Dagger.
+ *
+ * Two functions have been pre-created. You can modify, delete, or add to them,
+ * as needed. They demonstrate usage of arguments and return types using simple
+ * echo and grep commands. The functions can be called from the dagger CLI or
+ * from one of the SDKs.
+ *
+ * The first line in this comment block is a short description line and the
+ * rest is a long description with more detail on the module's purpose or usage,
+ * if appropriate. All modules should have a short description.
+ */
+import { dag, Directory, object, func, Container, Secret } from "@dagger.io/dagger"
+
+const baseImage = "mheers/opa-tools:latest"
+const registry = "registry-1.docker.io"
+const repository = "mheers/opa-policy"
+const tag = "1.0.0"
+const username = "mheers"
+
+@object()
+export class Ci {
+  async testRegos(directoryArg: Directory): Promise<string> {
+    return dag.container().from(baseImage)
+      .withMountedDirectory("/bundle", directoryArg)
+      .withWorkdir("/bundle")
+      .withExec(["opa", "test", "-v", "--coverage", "--format=json", "/bundle"]) // test
+      .stdout()
+  }
+
+  @func()
+  buildBundle(directoryArg: Directory): Container {
+    return dag.container().from(baseImage)
+      .withMountedDirectory("/bundle", directoryArg)
+      .withWorkdir("/bundle")
+      .withExec(["policy", "build", "/bundle", "--ignore", "*_test.rego", "-t", `${registry}/${repository}:${tag}`]) // build
+  }
+
+  @func()
+  async testBuildAndPushBundle(directoryArg: Directory, registryToken: Secret): Promise<string> {
+    await this.testRegos(directoryArg)
+    return this.buildBundle(directoryArg)
+      .withSecretVariable("REGISTRY_ACCESS_TOKEN", registryToken)
+      .withExec(["sh", "-c", `policy login -s ${registry} -u ${username} -p $REGISTRY_ACCESS_TOKEN`]) // login
+      .withExec(["policy", "push", `${registry}/${repository}:${tag}`]) // push
+      .stdout()
+  }
+}
